e60af7774507ca9c4411750a0dade444fd19e390
SharonJudith <jlalam@aetotechs.com>
2022-10-06 17:01:09 +0300

    success

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import home from './home.jpg';
+import Vector from './Vector.png';
+import rate from './rate.png';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={home} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={Vector} className="App-logo" alt="logo" /><p className="my-Crib">myCrib</p>
+      <p className="add-hostel">Add Hostel</p> 
+      <a href="App.js" className="login">Login</a>
+
+      <div className="vl"></div>
+
+      <a href="App.js" className="sign-up">Signup</a>
+        
       </header>
+
+      <body>
+      <img src={home} className="App-background" alt="background" />
+
+      <div className="box">Stays</div>
+      <div className="box1"> <img src={rate} className="Ratings"/>Ratings</div>
+      <div className="box2">Facilities</div>
+      <div className="box-content"></div>
+      </body>
     </div>
   );
 }
